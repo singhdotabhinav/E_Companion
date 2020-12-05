de19fe6cdcb7bed52d403d0edae6f77955a6512c
Abhinav Kumar <abhinav6400625@gmail.com>
2020-12-05 17:19:02 +0530

    third

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 //import 'package:E_Companion/Screens/Welcome/welcome_screen.dart';
 import 'package:E_Companion/HomeScreen/HomeScreen.dart';
 import 'package:E_Companion/Screens/Welcome/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'package:E_Companion/Screens/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
@@ -9,10 +10,19 @@ import 'constants.dart';
 
 
 
+
 //import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
 
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
